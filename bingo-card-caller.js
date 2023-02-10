c43559cdeef3d8cc9ab26cf71dd4a0f1d9ffbcea
Harvey Abaya <harvey@Harveys-MacBook-Pro.local>
2023-02-10 14:23:11 -0500

    docstrings for bingo-card-caller

--- bingo-card-caller.js
+++ bingo-card-caller.js
@@ -1,7 +1,15 @@
 const _ = require("lodash")
 const MAX_BINGO_NUMBER = 75
 
+/***
+ * BingoNumberCaller is a class that will represent the machine that will
+ * print out random numbers.
+ */
 class BingoNumberCaller {
+    /***
+     * This constructor will generate an array and shuffle it randomly
+     * with an O(N) operation where N is the length of the array (75)
+     */
     constructor() {
         // initialize a called numbers array of none.
         this.calledNumbers = [];
@@ -11,10 +19,23 @@ class BingoNumberCaller {
         this.bingoStack = _.shuffle(_.range(1, MAX_BINGO_NUMBER+1))
     }
 
+    /***
+     * This function will return of the numbers that have been called.
+     * 
+     * @returns an array of the numbers that have been called
+     */
     getCalledNumbers() {
         return this.calledNumbers;
     }
 
+    /***
+     * This function will call the next function. Since we have already
+     * randomly generated an array in initialization this function should be
+     * O(1) operation.
+     * 
+     * Returns: a number, or null if we have exhausted
+     * the list of distinct numbers.
+     */
     callNext() {
         if (this.bingoStack.length === 0) {
             console.log("No more numbers")
