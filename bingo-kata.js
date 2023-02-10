49ba8379731ac4a81be245d1e91c561b67d75f37
Harvey Abaya <harvey@Harveys-MacBook-Pro.local>
2023-02-10 13:14:34 -0500

    node package add

--- bingo-kata.js
+++ bingo-kata.js
@@ -1,5 +1,16 @@
-function startGame() {
-    console.log("To IMPLEMENT")
-}
+const prompt = require('prompt-sync')();
+const _ = require('lodash');
 
-startGame();
+function startGame() {
+    console.log("Welcome to a game of Bingo Kata")
+    console.log("Let's start by generating a bunch of bingo cards")
+    let numCards = null;
+    while(!numCards) {
+        numCards = Number(prompt("How many cards do you want to build?"))
+        if (isNaN(numCards) || !_.isPositive(numCards)) {
+            console.log("Please enter a valid positive number")
+            numCards = null;
+        }
+    }
+
+}
