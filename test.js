647efb54d0fa7482424590100313f0821cf081b8
Harvey Abaya <harvey@Harveys-MacBook-Pro.local>
2023-02-10 15:22:43 -0500

    Add following unit tests

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const { BingoNumberCaller, MAX_BINGO_NUMBER } = require("./bingo-card-caller")
+const { BingoCard } = require("./bingo-card-generator")
 const _ = require('lodash')
 
 describe("Test BingoNumberCaller", () => {
@@ -21,4 +22,33 @@ describe("Test BingoNumberCaller", () => {
     test("getCalledNumbers should equal the set of called numbers", () => {
         expect([...calledNumbers]).toEqual(bingoCaller.getCalledNumbers())
     })
+})
+
+describe("Test BingoCard", () => {
+    test("Mark number should mark a contained bingo number", () => {
+        const card = new BingoCard();
+        const { value } = card.numbers.values().next()
+        card.markNumber(value);
+
+        expect(card.markedNumbers.has(value))
+    })
+    
+    test("Mark number should not mark a non-existing card number", () => {
+        const card = new BingoCard()
+        const non_exisitng_num = _.find(
+            _.range(1, MAX_BINGO_NUMBER),
+            num => ![...card.numbers].includes(num)
+        )
+
+        card.markNumber(non_exisitng_num);
+        expect([...card.markedNumbers]).not.toContain(non_exisitng_num)
+    })
+
+    test("Card with all numbers marked should be a bingo", () => {
+        const card = new BingoCard();
+        expect(!card.isBingo())
+        card.markedNumbers = card.numbers;
+        expect(card.isBingo())
+    })
+
 })
