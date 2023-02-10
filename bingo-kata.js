9293d0e02c3dda0a229ee826d769e0af33231219
Harvey Abaya <harvey@Harveys-MacBook-Pro.local>
2023-02-10 13:32:07 -0500

    add switch statement display map

--- bingo-kata.js
+++ bingo-kata.js
@@ -1,5 +1,6 @@
 const prompt = require('prompt-sync')();
 const _ = require('lodash');
+const { BingoCard } = require("./bingo-card-generator");
 
 function startGame() {
     console.log("Welcome to a game of Bingo Kata")
@@ -13,4 +14,47 @@ function startGame() {
         }
     }
 
+    // generate cards
+    const cards = _.times(numCards, () => new BingoCard());
+
+    const actionMap = {
+        "X": "End Game",
+        "P": "print bingo cards",
+        "N": "call a bingo number",
+        "C": "check a given card",
+        "B": "check if any cards hav a bingo"
+    }
+
+    const displayActionMap = () => {
+        console.log("Please choose one of these actions:");
+        _.each(actionMap, (val, key) => {
+            console.log(`Enter ${key} to ${val}`)
+        })
+
+    }
+
+    displayActionMap();
+    let action = null;
+    while(!action === "X") {
+        let action = _.toUpper(prompt("what action do you want to perform?"))
+        if (!_.includes(_.keys(actionMap), action)) {
+            console.log("The action is not acceptable");
+            displayActionMap();
+            continue;
+        }
+        switch(action) {
+            case "P":
+                // todo
+                break;
+            case "N":
+                // todo
+                break;
+            case "C":
+                // todo
+                break;
+            case "B":
+                // todo
+                break;
+        }
+    }
 }
